52fd38ad94b9a194b995968a028a2352758a2999
ennet <ennet@mail.ru>
2016-02-19 13:55:05 +0300

    finished main page

--- __dev/scripts/production/modules/ui.js
+++ __dev/scripts/production/modules/ui.js
@@ -5,6 +5,10 @@ var ui = {
 
         this.initMainGallery();
 
+        this.sliderNews();
+
+        this.sliderActions();
+
     },
     phoneMask: function () {
         $("input.js-phone-mask").mask("+7(999) 999-99-99");
@@ -16,6 +20,28 @@ var ui = {
             mode : 'fade',
             speed : 700,
             auto : true
+        })
+    },
+    sliderNews: function(){
+        $('.js-list-news').bxSlider({
+            speed : 700,
+            pager: true,
+            moveSlides: 1,
+            minSlides: 2,
+            maxSlides: 2,
+            slideWidth: 470,
+            slideMargin: 20
+        })
+    },
+    sliderActions : function(){
+        $('.js-list-actions').bxSlider({
+            speed : 700,
+            moveSlides: 2,
+            minSlides: 4,
+            slideWidth: 370,
+            maxSlides: 4,
+            pager : true,
+            slideMargin: 0
         })
     }
 
